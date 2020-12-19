f58553eea3589c706916b52fd41112e282edd62a
doitwell <you@example.com>
2020-12-20 00:19:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,17 +4,8 @@ count = 0
 with open(r'C:\Users\57884\Desktop\11111111.txt','r') as f:
     for line in f.readlines():
         print(line.strip())
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
         print(count)
         count += 1
 
 
 print("hello, world")
->>>>>>> Stashed changes
-=======
-
-
-print("hello, world")
->>>>>>> d51e8325a73366b4b6b7736d94bfc7465cd8831a
