620d83fcdad69c82be6884370c5bcc1d0eb172e9
doitwell <you@example.com>
2020-12-19 23:50:14 +0800

    modify the input file name
    it's very important

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # _*_ coding: utf-8 _*_
 import os,sys
 
-with open(r'C:\Users\57884\Desktop\广州芯泰通信技术有限公司加班管理办法.docx','r') as f:
+with open(r'C:\Users\57884\Desktop\log4j.properties','r') as f:
     for line in f.readlines():
         print(line.strip())
