8928ac5e275bb04a9d9b95408a38e0f39f78b51e
doitwell <you@example.com>
2020-12-20 00:11:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 # _*_ coding: utf-8 _*_
 import os,sys
-
-with open(r'C:\Users\57884\Desktop\log4j.properties','r') as f:
+count = 0
+with open(r'C:\Users\57884\Desktop\11111111.txt','r') as f:
     for line in f.readlines():
         print(line.strip())
+<<<<<<< Updated upstream
+=======
+        print(count)
+        count += 1
+
+
+print("hello, world")
+>>>>>>> Stashed changes
